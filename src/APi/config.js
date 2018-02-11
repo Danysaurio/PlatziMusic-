0427405e291068a85e3a41cb8798a0c5a82913d1
JUAN DANIEL ZAVALA CASILLAS <daniel_zavala@bitbucket.org>
2018-02-11 16:06:40 -0600

    first comit

---
+++ src/APi/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: "PlatziMusic",
+  apiKey: "0af9f37a8ceb9c166c3c5c6413be5164",
+  secret: "db39afd51e914c5727347eb1cd878708",
+  registeredTo: "danysaurio"
+};
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+	#app
+		img(src="https://media.freepik.com/accounts/img/badges/you_are_on_top.png")
+		h1 PlatziMusic
+		ul
+			li(v-for="artist in artists") {{artist.name}}
+</template>
+
+<script>
+import getArtist from './api'
+export default {
+  name: "app",
+  data() {
+    return {
+     artists:[]
+    }
+  },
+  mounted: function(){
+    const self = this;
+    getArtist()
+      .then(function(artists){
+        console.log(artists)
+        self.artists = artists;
+      });
+  }
+};
+</script>
+
+<style lang="stylus">
+
+</style>
